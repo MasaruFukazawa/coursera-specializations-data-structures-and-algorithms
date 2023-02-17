01f51bdb60caca2ccaec54c9a502d7bb331025bf
Fukazawa Masaru <upw.ponsuke@gmail.com>
2023-02-17 14:55:32 +0900

    isort:検証

--- tests/coursera_specializations_data_structures_and_algorithms/algorithmic_toolbox/test_maximum_pairwise_product.py
+++ tests/coursera_specializations_data_structures_and_algorithms/algorithmic_toolbox/test_maximum_pairwise_product.py
@@ -5,6 +5,9 @@ from coursera_specializations_data_structures_and_algorithms.algorithmic_toolbox
     maximum_pairwise_product,
 )
 
+import sys
+
+sys.argv
 
 def test_maximum_pairwise_product():
     result = maximum_pairwise_product([1, 2, 3])
